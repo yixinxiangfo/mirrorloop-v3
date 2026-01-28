b2a3e309d68f95d203e84fa07daa9763ed143578
松尾絵美 <matsuoemi@matsuoemi-no-MacBook-Pro.local>
2026-01-28 10:14:26 +0900

    Day 3: 暗号化・復号化関数追加(56行)

--- index.js
+++ index.js
@@ -8,6 +8,38 @@ const pool = new Pool({
   }
 });
 
+//暗号化関数（encrypt）
+
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
+const ALGORITHM = 'aes-256-cbc'
+
+function encrypt(text){
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(
+    ALGORITHM,
+    Buffer.from(ENCRYPTION_KEY,'hex'),
+    iv
+    );
+  let encrypted = cipher.update(text,'utf8','hex');
+  encrypted += cipher.final('hex');
+  return iv.toString('hex') + ':' + encrypted;
+}
+
+// 復号化関数（decrypt）
+function decrypt(encryptedText){
+  const parts = encryptedText.split(':');
+  const iv = Buffer.from(parts[0],'hex');
+  const encryptedData = parts[1];
+  const decipher = crypto.createDecipheriv(
+    ALGORITHM,
+    Buffer.from(ENCRYPTION_KEY,'hex'),
+    iv
+    );
+  let decrypted = decipher.update(encryptedData,'hex','utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+}
+
 const express = require('express');
 const axios = require('axios');
 const crypto = require('crypto');
